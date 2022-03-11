4de5a12ededd7e7b3d00b484e62cb38ab387646a
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-11 20:15:08 +0530

    Regex to validate last name is updated

--- UserRegistration/src/com/Main.java
+++ UserRegistration/src/com/Main.java
@@ -1,19 +1,8 @@
 package com;
 
-
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-	    UserValidation userValidation = new UserValidation();
-        Scanner input = new Scanner(System.in);
-        String firstName = "";
-
-        System.out.print("Enter First Name: ");
-        if(userValidation.isFirstNameValid(input.nextLine())){
-            firstName = input.nextLine();
-        }
-
+    public static void main(String[] args) {
+	    new UserRegistration();
     }
 }
